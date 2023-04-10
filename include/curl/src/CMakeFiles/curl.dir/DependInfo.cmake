7da7d51b1c57c49d9714b8be2c24b35a2bb3b458
marti6j8 <marti6j8@mail.uc.edu>
2023-04-10 19:03:39 -0400

    Added Curl Library
    part 3/x

---
+++ include/curl/src/CMakeFiles/curl.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/workspaces/under-the-c-desktop/curl/lib/curl_multibyte.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/curl_multibyte.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/dynbuf.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/dynbuf.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/nonblock.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/nonblock.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/strtoofft.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/strtoofft.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/timediff.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/timediff.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/version_win32.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/version_win32.c.o"
+  "/workspaces/under-the-c-desktop/curl/lib/warnless.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/__/lib/warnless.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/slist_wc.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/slist_wc.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_binmode.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_binmode.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_bname.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_bname.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_dbg.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_dbg.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_hdr.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_hdr.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_prg.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_prg.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_rea.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_rea.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_see.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_see.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cb_wrt.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cb_wrt.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_cfgable.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_cfgable.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_dirhie.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_dirhie.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_doswin.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_doswin.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_easysrc.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_easysrc.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_filetime.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_filetime.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_findfile.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_findfile.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_formparse.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_formparse.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_getparam.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_getparam.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_getpass.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_getpass.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_help.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_help.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_helpers.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_helpers.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_hugehelp.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_hugehelp.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_libinfo.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_libinfo.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_listhelp.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_listhelp.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_main.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_main.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_msgs.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_msgs.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_operate.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_operate.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_operhlp.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_operhlp.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_paramhlp.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_paramhlp.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_parsecfg.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_parsecfg.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_progress.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_progress.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_setopt.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_setopt.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_sleep.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_sleep.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_stderr.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_stderr.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_strdup.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_strdup.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_urlglob.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_urlglob.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_util.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_util.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_vms.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_vms.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_writeout.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_writeout.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_writeout_json.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_writeout_json.c.o"
+  "/workspaces/under-the-c-desktop/curl/src/tool_xattr.c" "/workspaces/under-the-c-desktop/curl/src/CMakeFiles/curl.dir/tool_xattr.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "HAVE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "lib"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/under-the-c-desktop/curl/lib/CMakeFiles/libcurl.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
