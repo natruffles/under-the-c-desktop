7704e5f1874a754e90748f1a0e83525bb6dc4e6c
painteat <august.painter@gmail.com>
2023-04-11 00:34:05 -0400

    Format desktop code
    Redone adjustments to keep with the Google styling guide.

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -9,36 +9,24 @@
 using namespace std;
 using json = nlohmann::json;
 
-// This section details the operation of the main menu.  It takes no arguments and returns none.
-
-void Menu::menuNavigator() {
+//The MenuNavigator function is the only function called in main, and contains a loop that will run until exited with the RedoChecker function.
+void Menu::MenuNavigator() {
   int redo = 1;
-
-  // Implement Incorrect and Escape
-
-  // This section asks the user to make a function choice from the menu, storing their choice in the "menuChoice" variable.
-
   while (redo == 1) {
     cout << "Welcome to Weather ESP Light." << endl;
-    cout << "Please input the number corresponding to the desired operation:"
-         << endl;
-
+    cout << "Please input the number corresponding to the desired operation:" << endl;
     cout << "1. Enter Wifi SSID and Password" << endl;
     cout << "2. Enter Location" << endl;
-
-    cin >> menuChoice;
-
-// menuChoice is used in this switch statement to carry out the function selected by the user.
-
-    switch (menuChoice) {
+    cin >> menu_choice;
+    switch (menu_choice) {
       case 1:
-        getWifi();
-        redo = redoChecker();
+        GetWifi();
+        redo = RedoChecker();
         break;
 
       case 2:
-        getLocation();
-        redo = redoChecker();
+        GetLocation();
+        redo = RedoChecker();
         break;
 
       default:
@@ -49,31 +37,30 @@ void Menu::menuNavigator() {
   }
 }
 
-// mutator function for the SSID and the password; gets wifiSSID and wifiPassword from the user after prompts
-
-void Menu::getWifi() {
+//The GetWifi function retrieves the SSID and the password from the user after prompting them and stores them in the corresponding variables.
+void Menu::GetWifi() {
   cout << "Enter Wifi SSID:" << endl;
-  cin >> wifiSSID;
+  cin >> wifi_ssid;
   cout << "Enter Wifi Password:" << endl;
-  cin >> wifiPassword;
+  cin >> wifi_password;
 }
 
-// mutator function for zip code; sets locationZIP to value entered by user after prompt
-
-void Menu::getLocation() {
+//The GetLocation function retrieves the ZIP code of the device from the user after prompting them and stores it in the corresponding variable.
+void Menu::GetLocation() {
   cout << "Enter ZIP code:" << endl;
   cin >> locationZIP;
 }
 
-int Menu::redoChecker(){
-    string tempChoice;
+//The RedoChecker function determines if the user wants to go through the menu again, or exit the program.  It will exit the program if the user gives an invalid input.
+int Menu::RedoChecker(){
+    string temp_choice;
     cout << "Would you like to enter the menu again? (Y/N)" << endl;
-    cin >> tempChoice;
+    cin >> temp_choice;
     
-    if (tempChoice == "Y"){
+    if (temp_choice == "Y"){
         return 1;
     }
-    else if (tempChoice == "N")
+    else if (temp_choice == "N")
     {
         return 0;
     }
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ using json = nlohmann::json;
 int main()
 {
     Menu menu;
-    menu.menuNavigator();
+    menu.MenuNavigator();
 
     return 0;
 }
